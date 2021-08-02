ddb40564eccd5aa630af2bec66327f31bba2e30d
Xenia Filippova <32355526+riva-sm@users.noreply.github.com>
2021-08-03 02:04:17 +0800

    мобильная адаптация главной страницы

--- js/main.js
+++ js/main.js
@@ -5,6 +5,17 @@
         freeMode: true,
         watchSlidesVisibility: true,
         watchSlidesProgress: true,
+        breakpoints: {
+          640: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 5,
+            spaceBetween: 10,
+          },
+        }
+        
       });
       var swiper2 = new Swiper(".mySwiper2", {
         loop: true,
@@ -12,4 +23,16 @@
         thumbs: {
           swiper: swiper,
         },
+                
+
       });
+
+const menuBtn = document.querySelector(".menu-button");
+const menu = document.querySelector(".navbar-pannel");
+const menuCloseBtn = document.querySelector(".close-menu");
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("is-open");
+});
+menuCloseBtn.addEventListener("click", () => {
+  menu.classList.toggle("is-open");
+});
